41eed49dc08bf362460fd2b02ede079504b25198
Mirko Iannella <mirko@iannella.sh>
2016-07-21 12:57:07 +0200

    Implemented localStorage cart

---
+++ front/public/js/cart.js
@@ -0,0 +1,24 @@
+function add_to_cart( product, quantity ) {
+    // Get the current cart
+    cart = JSON.parse( localStorage.getItem("cart") );
+
+    // Is the product already in the cart?
+    if( cart && cart[product] ) {
+        // Add one
+        cart[product]["quantity"]++;
+        localStorage.setItem( "cart", JSON.stringify(cart) );
+    } else {
+        // Store it
+        var cart = {};
+        cart[product] = {};
+        cart[product]["quantity"] = quantity;
+
+        localStorage.setItem( "cart", JSON.stringify(cart) );
+    }
+
+    // Log
+    //console.log( JSON.parse( localStorage.getItem("cart") ) );
+    console.log( localStorage.getItem("cart") );
+
+    //localStorage.clear();
+}
